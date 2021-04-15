90cdda4ed1d6fbc77f5cad8488f248d0031aa809
Thierry Bitar <bitarthierry@gmail.com>
2021-04-15 21:01:03 +0200

    improved getRandomColor method and use colors.xml

--- app/src/main/java/com/example/maru/controller/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/maru/controller/RecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.maru.controller;
 
+import android.content.res.Resources;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -9,7 +11,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.maru.R;
@@ -17,6 +23,7 @@ import com.example.maru.di.DI;
 import com.example.maru.model.Meeting;
 
 import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,26 +40,39 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.meeting_list_item, parent, false);
-            return new ViewHolder(itemView);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.meeting_list_item, parent, false);
+        return new ViewHolder(itemView);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Meeting meeting = listOfMeeting.get(position);
 
         holder.mEmailList.setText(getEmailParticipantList(meeting));
         holder.mItemListName.setText(getTitleOfMeeting(meeting));
-        holder.mItemOfListOfMeeting.setColorFilter(R.color.purple_700);
+        holder.mItemOfListOfMeeting.setColorFilter(getRandomColor(holder.itemView.getResources()));
         holder.mItemDeleteButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(holder.itemView.getContext(), "Réunion supprimé", Toast.LENGTH_SHORT).show();
                 DI.getMeetingService().deleteMeeting(meeting);
-                notifyItemRemoved(position);
+                notifyDataSetChanged();
             }
         });
 
+    }
+
+    @ColorInt
+    int getRandomColor(Resources resources) {
+        @ColorRes int[] colorArray = {R.color.ROSE, R.color.MAUVE, R.color.BLUE, R.color.VIOLET, R.color.ROUGE, R.color.BLEUCIEL,
+                                      R.color.VERTCLAIR, R.color.JAUNEBEIGE, R.color.BEIGE, R.color.JAUNEORANGE, R.color.SAUMON,
+                                      R.color.ORANGE, R.color.CYAN  };
+
+        int randomIndex = new Random().nextInt(colorArray.length);
+        @ColorRes int randomColorResId = colorArray[randomIndex];
+        @ColorInt int randomColorInt = ResourcesCompat.getColor(resources, randomColorResId, null);
+        return randomColorInt;
     }
 
     private String getTitleOfMeeting(Meeting meeting) {
@@ -66,8 +86,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
      */
     private String getEmailParticipantList(Meeting meeting) {
         StringBuilder emailsOfParticipants = new StringBuilder();
-        for(String mail:meeting.getListOfParticipants()){
-            emailsOfParticipants.append(mail).append(","+" ");
+        for (String mail : meeting.getListOfParticipants()) {
+            emailsOfParticipants.append(mail).append("," + " ");
         }
         return emailsOfParticipants.toString();
     }
